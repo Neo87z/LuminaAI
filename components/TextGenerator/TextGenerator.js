85e0d49d8fc1e85c7f00b3b633fedc1edc996bf5
Neo87z <shehanhoradagoda12@gmail.com>
2024-04-05 22:35:18 -0230

    main

--- components/TextGenerator/TextGenerator.js
+++ components/TextGenerator/TextGenerator.js
@@ -195,7 +195,7 @@ const ImageGenerator = () => {
         </div>
       )}
 
-      <div className="rbt-static-bar" style={{ transform: "translateX(-12.5%)" }}>
+      <div className="rbt-static-bar" >
         <Tooltip id="my-tooltip" className="custom-tooltip tooltip-inner" />
         <form className="new-chat-form border-gradient">
           <textarea
